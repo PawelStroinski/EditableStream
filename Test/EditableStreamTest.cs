ad8301386bba6ea4c4bc7c7c0d3ab9d8fa0d2c1e
Paweł Stroiński <pol84@live.com>
2014-06-02 08:09:13 +0100

    Add library and test

---
+++ Test/EditableStreamTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace EditableStream
+{
+    public class EditableStreamTest
+    {
+        [Test]
+        public void DemoInLoop()
+        {
+            var filename = Path.GetTempPath() + "sample.txt";
+            if (File.Exists(filename))
+                File.Move(filename, filename + Guid.NewGuid());
+            var count = 10;
+            var expectedLine = "This is marvelously very nice test." + Environment.NewLine;
+            var expected = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                Program.Main(null);
+                expected += expectedLine;
+                var actual = File.ReadAllText(filename);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
